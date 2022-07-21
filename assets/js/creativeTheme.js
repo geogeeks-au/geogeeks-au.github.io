0e40993461694ba43e394dc95a6ab61604625042
Sam Wilson <sam@samwilson.id.au>
2022-07-21 15:51:31 +0800

    Let animated scroll still change location hash
    The #events etc. links are useful to have in the URL, when people
    click on the navbar links.
    
    Also remove some commented out code, and fix indenting of file
    header.

--- assets/js/creativeTheme.js
+++ assets/js/creativeTheme.js
@@ -1,9 +1,9 @@
-/*!
-    * Start Bootstrap - Creative v6.0.4 (https://startbootstrap.com/theme/creative)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-    */
-    (function($) {
+/**
+ * Start Bootstrap - Creative v6.0.4 (https://startbootstrap.com/theme/creative)
+ * Copyright 2013-2020 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+ */
+(function($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
@@ -15,7 +15,6 @@
         $('html, body').animate({
           scrollTop: (target.offset().top - 72)
         }, 1000, "easeInOutExpo");
-        return false;
       }
     }
   });
@@ -43,21 +42,5 @@
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
-
-  // Magnific popup calls
-  // $('#portfolio').magnificPopup({
-  //   delegate: 'a',
-  //   type: 'image',
-  //   tLoading: 'Loading image #%curr%...',
-  //   mainClass: 'mfp-img-mobile',
-  //   gallery: {
-  //     enabled: true,
-  //     navigateByImgClick: true,
-  //     preload: [0, 1]
-  //   },
-  //   image: {
-  //     tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-  //   }
-  // });
 
 })(jQuery); // End of use strict
